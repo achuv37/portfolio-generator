1195428b4be32c4f50bb55ae4771dd7fc40aa8e2
Achu Ajesh <achuv37@gmail.com>
2022-04-01 20:20:42 -0400

    Generated html file

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2,process.argv.length);
-console.log(profileDataArgs);
+//console.log(profileDataArgs);
+//const name = profileDataArgs[0];
+//const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 
-// function for profile data
-const printProfileData = profileDataArr => {
-  for(let i=0; i< profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
-  
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
 
-}
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name,github), err => {
+  if (err) throw new Error(err);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
