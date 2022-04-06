583e6050f48aace444df819fe949ef38b2fe32e3
Achu Ajesh <achuv37@gmail.com>
2022-04-06 09:48:10 -0400

    updated template

--- app.js
+++ app.js
@@ -3,13 +3,6 @@ const inquirer = require('inquirer');
 
 const generatePage = require('./src/page-template.js');
 
-//const pageHTML = generatePage(name, github);
-
-//fs.writeFile('./index.html', pageHTML, err => {
-  //if (err) throw err;
-  //console.log('Portfolio complete! Check out index.html to see the output!');
-//});
-//console.log(inquirer);
 const promptUser = () => {
   return inquirer.prompt([
   {
@@ -138,5 +131,11 @@ return inquirer.prompt([
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-  console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+     fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+
+       console.log('Page created! Check out index.html in this directory to see it!');
+    });
 });
